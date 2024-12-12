d1576a98bf3cb8919374c87d0f40fd6eea21acd7
aendyp <43209125+aendyp@users.noreply.github.com>
2024-12-13 00:59:24 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+# Opprett et virtuelt miljø
 python3 -m venv venv
 source venv/bin/activate
+
+# Installer nødvendige Python-pakker
 pip install -r requirements.txt
+
+# Last ned spaCy-modellen for norsk
 python -m spacy download nb_core_news_sm --direct-download
+
+# Alternativt kan du bruke denne linjen for å spesifisere en lokal bane hvis nedlastingen feiler
+# python -m spacy link nb_core_news_sm /path/to/model/directory --force
