099da91a297120273ca1e902db67efb18c1828f5
aendyp <43209125+aendyp@users.noreply.github.com>
2024-12-13 01:14:18 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,7 @@ pip install -r requirements.txt
 
 # Last ned spaCy-modellen for norsk
 python -m spacy download nb_core_news_sm --direct-download
+
+# Sjekk at modellen er lastet ned
+echo "Verifiserer at modellen er lastet ned..."
+ls /usr/local/lib/python3.10/dist-packages/spacy/data/
