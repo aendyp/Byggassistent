cacea675fb8c2d22b53c50c76099e3dfbb945d7c
aendyp <43209125+aendyp@users.noreply.github.com>
2024-12-13 01:28:40 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,17 +6,13 @@ source venv/bin/activate
 # Installer nødvendige Python-pakker
 pip install -r requirements.txt
 
-# Last ned spaCy-modellen for norsk
+# Last ned spaCy-modellen for norsk til en spesifikk lokal mappe
 python -m spacy download nb_core_news_sm --direct-download
 
-# Sjekk om modellen er lastet ned ved å liste innholdet i spacy/data
-echo "Verifiserer at modellen er lastet ned..."
-ls /usr/local/lib/python3.10/dist-packages/spacy/data/
+# Flytt modellen til en bestemt mappe i prosjektet
+mkdir -p /app/models/nb_core_news_sm
+mv /usr/local/lib/python3.10/dist-packages/spacy/data/nb_core_news_sm /app/models/nb_core_news_sm
 
-# Sjekk om modellen finnes i katalogen
-if [ -d "/usr/local/lib/python3.10/dist-packages/spacy/data/nb_core_news_sm" ]; then
-  echo "Modellen er lastet ned og funnet."
-else
-  echo "Modellen ble ikke funnet!"
-  exit 1
-fi
+# Sjekk at modellen er lastet ned og flyttet korrekt
+echo "Verifiserer at modellen er flyttet..."
+ls /app/models/nb_core_news_sm
