d6aeed74483d7871f35ae2509df286b8d6653f49
aendyp <43209125+aendyp@users.noreply.github.com>
2024-12-13 01:01:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,5 @@ source venv/bin/activate
 # Installer nødvendige Python-pakker
 pip install -r requirements.txt
 
-# Last ned spaCy-modellen for norsk
+# Last ned spaCy-modellen for norsk til en spesifikk lokal mappe
 python -m spacy download nb_core_news_sm --direct-download
-
-# Alternativt kan du bruke denne linjen for å spesifisere en lokal bane hvis nedlastingen feiler
-# python -m spacy link nb_core_news_sm /path/to/model/directory --force
