ac7cecb285c31525f537d52e5b85dd958e2043c8
aendyp <43209125+aendyp@users.noreply.github.com>
2024-12-15 14:51:25 +0100

    Update qa_utils.py

--- utils/qa_utils.py
+++ utils/qa_utils.py
@@ -1,7 +1,7 @@
 from langchain.chains import ConversationalRetrievalChain
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.chat_models import ChatOpenAI
 import os
 import logging
 
