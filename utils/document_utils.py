afc4ca7decece2c4d1068e38134ee25c5ff2b740
aendyp <43209125+aendyp@users.noreply.github.com>
2024-12-15 10:59:45 +0100

    Update document_utils.py

--- utils/document_utils.py
+++ utils/document_utils.py
@@ -19,32 +19,19 @@ def load_documents():
         loader_arbeidsplass = PyPDFLoader("inndata/Arbeidsplassforskriften.pdf")
         loader_arbeid = PyPDFLoader("inndata/Forskrift_om_utførelse_av_arbeid.pdf")
 
-        docs_tek17 = loader_tek17.load_and_split()
-        docs_pbl = loader_pbl.load_and_split()
-        docs_aml = loader_aml.load_and_split()
-        docs_sak10 = loader_sak10.load_and_split()
-        docs_dok = loader_dok.load_and_split()
-        docs_bhf = loader_bhf.load_and_split()
-        docs_breeam = loader_breeam.load_and_split()
-        docs_avfall = loader_avfall.load_and_split()
-        docs_internkontroll = loader_internkontroll.load_and_split()
-        docs_forurensning = loader_forurensning.load_and_split()
-        docs_arbeidsplass = loader_arbeidsplass.load_and_split()
-        docs_arbeid = loader_arbeid.load_and_split()
-
         return {
-            "TEK17": docs_tek17,
-            "PBL": docs_pbl,
-            "AML": docs_aml,
-            "SAK10": docs_sak10,
-            "DOK": docs_dok,
-            "Byggherreforskriften": docs_bhf,
-            "BREEAM": docs_breeam,
-            "Avfall": docs_avfall,
-            "Internkontroll": docs_internkontroll,
-            "Forurensning": docs_forurensning,
-            "Arbeidsplass": docs_arbeidsplass,
-            "Arbeid": docs_arbeid
+            "TEK17": loader_tek17.load_and_split(),
+            "PBL": loader_pbl.load_and_split(),
+            "AML": loader_aml.load_and_split(),
+            "SAK10": loader_sak10.load_and_split(),
+            "DOK": loader_dok.load_and_split(),
+            "Byggherreforskriften": loader_bhf.load_and_split(),
+            "BREEAM": loader_breeam.load_and_split(),
+            "Avfall": loader_avfall.load_and_split(),
+            "Internkontroll": loader_internkontroll.load_and_split(),
+            "Forurensning": loader_forurensning.load_and_split(),
+            "Arbeidsplass": loader_arbeidsplass.load_and_split(),
+            "Arbeid": loader_arbeid.load_and_split()
         }
     except Exception as e:
         logger.error(f"Feil under lasting av dokumenter: {e}")
